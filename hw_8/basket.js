365043c708e2671973d2cc0b9393798cd02ffc8e
aleksandrdiptan <diptan@huntflow.ru>
2022-02-05 09:26:03 +0300

    hw_8 initial commit

---
+++ hw_8/basket.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const basketCountSpan = document.querySelector('.cartIconWrap span')
+const basketTotalSpanValue = document.querySelector('.basketTotalValue')
+
+document.querySelector('.cartIconWrap').
